c37ccefec727883dfafb7f8f545f182c1427a0fd
MUHAMMAD_AJMAL <muhammadajmalvn@gmail.com>
2022-12-21 00:38:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import NavBar from './Components/NavBar/NavBar';
+import { action, originals,horror,comedy} from './urls';
+import './App.css';
+import Banner from './Components/Banner/Banner';
+import RowPost from './Components/RowPost/RowPost';
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <Banner />
+      <RowPost url={originals} title='Netflix Originals'/>
+      <RowPost url={action} title='Action' isSmall/>
+      <RowPost url={horror} title='Horror' isSmall/>
+      <RowPost url={comedy} title='Comedy' isSmall/>
+    </div>
+  );
+}
+
+export default App;
